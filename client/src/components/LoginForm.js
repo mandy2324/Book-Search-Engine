c8718cf7f97f536b789a0f0c03200e2a844ca081
unknown <jasdeepsaini30@gmail.com>
2022-01-13 15:54:54 -0500

    adding changes to server

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -5,11 +5,18 @@ import { Form, Button, Alert } from 'react-bootstrap';
 import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
 
+
+
+
+
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
 
+
+
+  
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setUserFormData({ ...userFormData, [name]: value });
