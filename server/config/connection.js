4c9e64b2d61ce136b0ca5823bbee14b59505cee9
unknown <jasdeepsaini30@gmail.com>
2022-01-24 19:22:36 -0500

    adding bug fixes for connection config changes

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 const string= process.env.MONGODB_URI || 'mongodb://localhost/googlebooks'
-console.log(string)
 mongoose.connect(string, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
